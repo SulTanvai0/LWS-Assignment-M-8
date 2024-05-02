7bad056a13db7fb072d4ca22174c7a018dbea729
SulTanvai0 <MuhammedMehedih@gmail.com>
2024-05-02 12:16:56 +0600

    updating home page

--- components/NavFiles/UserInfo.jsx
+++ components/NavFiles/UserInfo.jsx
@@ -2,10 +2,12 @@
 
 import { useAuth } from "@/Hooks/useAuth";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const UserInfo = () => {
 
     const { auth, setAuth } = useAuth();
+    const pathname = usePathname()
 
     const handelLogOut = () => {
         setAuth(null)
@@ -15,7 +17,9 @@ const UserInfo = () => {
     if (auth?.id) {
         return (
             <li className="flex gap-2">
-                <p className="py-2 font-bold  "> Hello, {auth && auth?.firstName}</p>
+                {
+                    pathname === "/" && <p className="py-2 font-bold  "> Hello, {auth && auth?.firstName}</p>
+                }
                 <button onClick={handelLogOut} className="py-2 bg-[#eb4a36] px-6 rounded-md text-white content-center">
                     LogOut
                 </button>
